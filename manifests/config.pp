61df4b0b1f55c813a8b7b6dc9efc4c1023862e83
Ivan Berezovskiy <iberezovskiy@mirantis.com>
2013-08-20 19:37:00 +0400

    Use command 'echo' instead of 'sed'

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,8 +1,8 @@
 class savanna::config (
   $horizon_file       = '/usr/share/openstack-dashboard/openstack_dashboard/settings.py',
   $horizon_local_file = "/usr/share/openstack-dashboard/openstack_dashboard/local/local_settings.py",
-  $run_command_f      = "sed -i -e \"s/'dashboards': ('project', 'admin',),/\'dashboards\': (\'project\', \'admin\', \'savanna\'),/g\"",
-  $run_command_s      = "sed -i -e \"s/INSTALLED_APPS = (/INSTALLED_APPS = (\\n    \'savannadashboard\',/g\"",
+  $run_command_f      = "echo \"HORIZON_CONFIG['dashboards'] += ('savanna',)\"",
+  $run_command_s      = "echo \"INSTALLED_APPS += ('savannadashboard',)\"",
   $savanna_url = 'SAVANNA_URL = http://localhost:8386/v1.0'
 ) {
 
@@ -17,8 +17,8 @@ class savanna::config (
   }
 
   exec { "Editing horizon config":
-    command => "${run_command_f} ${horizon_file} \
-                && ${run_command_s} ${horizon_file} \
+    command => "${run_command_f} >> ${horizon_file} \
+                && ${run_command_s} >> ${horizon_file} \
                 && echo ${savanna_url} >> ${horizon_local_file}",
     path    => ["/bin"],
   }
